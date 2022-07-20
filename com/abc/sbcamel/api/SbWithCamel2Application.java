a65c60dcf39c4b49cd67364df85ce730a14775e8
ram569708 <109209979+ram569708@users.noreply.github.com>
2022-07-20 15:07:41 +0530

    Create SbWithCamel2Application.java
    Added springboot entry file

---
+++ com/abc/sbcamel/api/SbWithCamel2Application.java
@@ -0,0 +1,13 @@
+package com.abc.sbcamel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbWithCamel2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbWithCamel2Application.class, args);
+	}
+
+}
